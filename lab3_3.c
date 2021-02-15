c5a4b4be05432f4aa3fb73cd1da92881bab046a1
Peerawat Wongmek <peerawat.won@ku.th>
2021-02-15 17:36:20 +0700

    Create lab3_3.c

---
+++ lab3_3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=2;
+    scanf("%d",&n);
+    while(n!=1)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)
+            {
+                printf("%d\n",i);
+                n/=i;
+            }
+        }
+        i++;
+    }
+    return 0;
+}
