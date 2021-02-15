b6b4e42436304e440a3f5396473b3376b1843638
Peerawat Wongmek <peerawat.won@ku.th>
2021-02-15 19:08:48 +0700

    add

--- lab3_2.c
+++ lab3_2.c
@@ -3,24 +3,31 @@ int main()
 {
     int i,j,n,max=0,x;
     scanf("%d",&n);
-    int p[n],a[n];
-    for(i=0;i<n;i++)
+    if(n>=1&&n<=10000)
     {
-        scanf("%d",&p[i]);
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-            if(p[i]==p[i+1])
-            {
-                a[i]+=a[i+1];
-        }
-       if(a[i]>max)
+        int p[n],a[n];
+        for(i=0; i<n; i++)
+            scanf("%d %d",&p[i],&a[i]);
+        for(i=0; i<n; i++)
         {
-            max=a[i];
-            x=i+1;
+            if(p[i]>=1&&p[i]<=48&&a[i]>=1&&a[i]<=50000)
+            {
+                for(i=0; i<n; i++)
+                {
+                    if(p[i]==p[i+1])
+                    {
+                        a[i]+=a[i+1];
+                    }
+                    if(a[i]>max)
+                    {
+                        max=a[i];
+                        x=i+1;
+                    }
+                }
+            }
         }
+        printf("%d %d",x,max);
     }
-    printf("%d %d",x,max);
+
     return 0;
 }
---
+++ lab3_4.c
@@ -0,0 +1,23 @@
+int main()
+{
+  int n, i = 3, count, c,max;
+  scanf("%d", &n);
+  for (count = 2; count <= n;)
+  {
+    for (c = 2; c <= n; c++)
+    {
+      if (i%c == 0)
+        break;
+    }
+    if (c == i)
+    {
+      max=i;
+      count++;
+    }
+    if(i==n)
+        break;
+    i++;
+  }
+    printf("%d",max);
+  return 0;
+}
